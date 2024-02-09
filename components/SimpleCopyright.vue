8d7ec612e2e01ebed889d5d90d40f23f6b4229ec
Kotomi <40010177+acgtap@users.noreply.github.com>
2024-02-09 16:08:45 +0800

    Update SimpleCopyright.vue

--- components/SimpleCopyright.vue
+++ components/SimpleCopyright.vue
@@ -1,18 +1,11 @@
 <script lang="ts" setup>
-import pkg from '~/package.json'
 </script>
 
 <template>
   <div text="center sm" my-3>
     <CurrentVersion />
     <div flex="~" items-center justify-center gap="2">
-      <a
-        :href="" target="_blank"
-        class="inline-flex items-center justify-center"
-      >
-        <div i-ri-github-line mr-1 />
-        <span>Code</span>
-      </a>
+      
       by
       <a
         href="https://qm.qq.com/q/jhzFDyBPz4" target="_blank"
